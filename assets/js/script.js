f339df714a8b49c59fdb6b809819ad9918c8843b
Sultan Bakhytzhanov <bahytzanovsultan@gmail.com>
2023-01-05 23:50:17 +0600

    Completed header

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,7 @@ VANTA.WAVES({
 	scaleMobile: 1.0,
 	color: 0x0,
 })
+window.addEventListener('scroll', function () {
+	let nav = document.querySelector('nav')
+	nav.classList.toggle('sticky', window.scrollY > 0)
+})
