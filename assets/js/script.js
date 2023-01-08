35643e0a239013df15d738fb7ddc9a9375f4e9f9
Sultan Bakhytzhanov <bahytzanovsultan@gmail.com>
2023-01-08 18:59:17 +0600

    Minor changes/Finished layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,17 @@ VANTA.WAVES({
 	scaleMobile: 1.0,
 	color: 0x0,
 })
+VANTA.WAVES({
+	el: '#intro2',
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.0,
+	minWidth: 200.0,
+	scale: 1.0,
+	scaleMobile: 1.0,
+	color: 0x0,
+})
 window.addEventListener('scroll', function () {
 	let nav = document.querySelector('nav')
 	nav.classList.toggle('sticky', window.scrollY > 0)
